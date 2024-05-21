41a5a85fb54207bc75895e0fd55a6915f43b195a
Louis Leung <105177042+lmclouis@users.noreply.github.com>
2024-05-21 16:22:07 -0700

    minor  update

--- btmc/__init__.py
+++ btmc/__init__.py
@@ -1,7 +1,1 @@
 from .utils import bt
-
-# __version__ = '0.0.1'
-
-
-# def backtesting_backtesting(resolution, delay, data, model, window, threshold, long_or_short, direction, tc):
-#     return utils._backtesting(resolution, delay, data, model, window, threshold, long_or_short, direction, tc)
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as file:
 
 setuptools.setup(
     name='btmc',  # this should be unique
-    version='0.0.3',
+    version='0.0.4',
     author='mc',
     description='backtesing package',
     packages=setuptools.find_packages(),
